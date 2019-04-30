e5804a9f0590384323170e648a366c5d82f29690
Oğuzhan Baran <oguzhan.baran96@gmail.com>
2019-04-30 18:53:18 +0300

    Initial commit

---
+++ oguzhan/Form1.Designer.cs
@@ -0,0 +1,216 @@
+﻿namespace oguzhan
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.nudSleep = new System.Windows.Forms.NumericUpDown();
+            this.nudMin = new System.Windows.Forms.NumericUpDown();
+            this.label3 = new System.Windows.Forms.Label();
+            this.nudMax = new System.Windows.Forms.NumericUpDown();
+            this.btnRandom = new System.Windows.Forms.Button();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.pnlButtons = new System.Windows.Forms.FlowLayoutPanel();
+            this.tmr = new System.Windows.Forms.Timer(this.components);
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.nudSleep)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudMin)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudMax)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.PaleGoldenrod;
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel1.Controls.Add(this.btnClear);
+            this.panel1.Controls.Add(this.btnStart);
+            this.panel1.Controls.Add(this.btnRandom);
+            this.panel1.Controls.Add(this.nudMax);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.nudMin);
+            this.panel1.Controls.Add(this.nudSleep);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Location = new System.Drawing.Point(12, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(275, 127);
+            this.panel1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 14);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(111, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Bekleme Süresi (ms) =";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(47, 39);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(76, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Değer Aralığı =";
+            // 
+            // nudSleep
+            // 
+            this.nudSleep.Location = new System.Drawing.Point(125, 12);
+            this.nudSleep.Maximum = new decimal(new int[] {
+            100000,
+            0,
+            0,
+            0});
+            this.nudSleep.Name = "nudSleep";
+            this.nudSleep.Size = new System.Drawing.Size(56, 20);
+            this.nudSleep.TabIndex = 2;
+            this.nudSleep.Value = new decimal(new int[] {
+            500,
+            0,
+            0,
+            0});
+            // 
+            // nudMin
+            // 
+            this.nudMin.Location = new System.Drawing.Point(125, 37);
+            this.nudMin.Maximum = new decimal(new int[] {
+            100000,
+            0,
+            0,
+            0});
+            this.nudMin.Name = "nudMin";
+            this.nudMin.Size = new System.Drawing.Size(56, 20);
+            this.nudMin.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(187, 39);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(10, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "-";
+            // 
+            // nudMax
+            // 
+            this.nudMax.Location = new System.Drawing.Point(203, 37);
+            this.nudMax.Name = "nudMax";
+            this.nudMax.Size = new System.Drawing.Size(56, 20);
+            this.nudMax.TabIndex = 5;
+            this.nudMax.Value = new decimal(new int[] {
+            100,
+            0,
+            0,
+            0});
+            // 
+            // btnRandom
+            // 
+            this.btnRandom.Location = new System.Drawing.Point(137, 63);
+            this.btnRandom.Name = "btnRandom";
+            this.btnRandom.Size = new System.Drawing.Size(109, 23);
+            this.btnRandom.TabIndex = 6;
+            this.btnRandom.Text = "Random Üret";
+            this.btnRandom.UseVisualStyleBackColor = true;
+            this.btnRandom.Click += new System.EventHandler(this.btnRandom_Click);
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(137, 92);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(109, 23);
+            this.btnStart.TabIndex = 7;
+            this.btnStart.Text = "Başla";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(15, 92);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(87, 23);
+            this.btnClear.TabIndex = 7;
+            this.btnClear.Text = "Temizle";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // pnlButtons
+            // 
+            this.pnlButtons.Location = new System.Drawing.Point(11, 145);
+            this.pnlButtons.Name = "pnlButtons";
+            this.pnlButtons.Size = new System.Drawing.Size(280, 210);
+            this.pnlButtons.TabIndex = 6;
+            // 
+            // tmr
+            // 
+            this.tmr.Tick += new System.EventHandler(this.tmr_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(299, 356);
+            this.Controls.Add(this.pnlButtons);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Quiz";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.nudSleep)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudMin)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudMax)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.NumericUpDown nudSleep;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.NumericUpDown nudMax;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.NumericUpDown nudMin;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Button btnRandom;
+        private System.Windows.Forms.FlowLayoutPanel pnlButtons;
+        private System.Windows.Forms.Timer tmr;
+    }
+}
+
---
+++ oguzhan/Form1.cs
@@ -0,0 +1,182 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace oguzhan
+{
+    public partial class Form1 : Form
+    {
+        private const int BUTTON_COUNT = 30;
+
+        private bool randomInitialized = false;
+
+        private Button lastActiveButton = null;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            for (int i = 0; i < BUTTON_COUNT; i++)
+            {
+                Button btn = new Button();
+                btn.Size = new Size(40, 35);
+                btn.Name = "button_" + i;
+                btn.BackColor = SystemColors.Control;
+                btn.Tag = i;
+                btn.Click += Btn_Click;
+
+                pnlButtons.Controls.Add(btn);
+            }
+        }
+
+        private void Btn_Click(object sender, EventArgs e)
+        {
+            Button button = (Button) sender;
+            if (button.Text.Length <= 0) return;
+
+            foreach (Button btn in pnlButtons.Controls)
+            {
+                btn.BackColor = SystemColors.Control;
+            }
+
+            button.BackColor = Color.Green;
+            lastActiveButton = button;
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            tmr.Stop();
+            randomInitialized = false;
+            lastActiveButton = null;
+            foreach (Button btn in pnlButtons.Controls)
+            {
+                btn.Text = "";
+                btn.BackColor = SystemColors.Control;
+            }
+        }
+
+        private void btnRandom_Click(object sender, EventArgs e)
+        {
+            if (tmr.Enabled) return;
+            int min = Convert.ToInt32(nudMin.Value);
+            int max = Convert.ToInt32(nudMax.Value);
+            if (min > max)
+            {
+                MessageBox.Show("Minimum değer maksimum değerden büyük olamaz!");
+                return;
+            }
+            if ((max - min) < BUTTON_COUNT)
+            {
+                MessageBox.Show("2 değer arasındaki fark minimum " + BUTTON_COUNT + " olmalıdır!");
+                return;
+            }
+
+            Random rnd = new Random();
+            List<int> randomNumbers = new List<int>();
+            for (int i = 0; i < BUTTON_COUNT; i++)
+            {
+                int random = rnd.Next(min, max);
+                while (randomNumbers.IndexOf(random) > -1)
+                {
+                    random = rnd.Next(min, max);
+                }
+                randomNumbers.Add(random);
+            }
+
+            for (int i = 0; i < BUTTON_COUNT; i++)
+            {
+                int randomNumber = randomNumbers[i];
+                pnlButtons.Controls["button_" + i].Text = randomNumber.ToString();
+            }
+
+            randomInitialized = true;
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            if (!randomInitialized)
+            {
+                MessageBox.Show("İlk önce random sayıları üretin!");
+                return;
+            }
+            if (lastActiveButton == null)
+            {
+                MessageBox.Show("Lütfen başlangıç noktası seçin!");
+                return;
+            }
+
+            tmr.Interval = Convert.ToInt32(nudSleep.Value);
+            tmr.Start();
+        }
+
+        private void tmr_Tick(object sender, EventArgs e)
+        {
+            List<String> nearbyButtons = new List<String>();
+
+            int activeIndex = Convert.ToInt32(lastActiveButton.Tag);
+            if ((activeIndex - 6) > -1)
+            {
+                Button btn = (Button) pnlButtons.Controls["button_" + (activeIndex - 6)];
+                if (btn.BackColor != Color.Red && btn.BackColor != Color.Green)
+                {
+                    nearbyButtons.Add("button_" + (activeIndex - 6));//Üstteki komşu
+                }
+            }
+            if ((activeIndex + 6) < BUTTON_COUNT)
+            {
+                Button btn = (Button)pnlButtons.Controls["button_" + (activeIndex + 6)];
+                if (btn.BackColor != Color.Red && btn.BackColor != Color.Green)
+                {
+                    nearbyButtons.Add("button_" + (activeIndex + 6));//Alttaki komşu
+                }
+            }
+            if ((activeIndex - 1) > -1 && (activeIndex % 6) != 0)
+            {
+                Button btn = (Button)pnlButtons.Controls["button_" + (activeIndex - 1)];
+                if (btn.BackColor != Color.Red && btn.BackColor != Color.Green)
+                {
+                    nearbyButtons.Add("button_" + (activeIndex - 1));//Soldaki komşu
+                }
+            }
+            if ((activeIndex + 1) < BUTTON_COUNT && ((activeIndex + 1) % 6) != 0)
+            {
+                Button btn = (Button)pnlButtons.Controls["button_" + (activeIndex + 1)];
+                if (btn.BackColor != Color.Red && btn.BackColor != Color.Green)
+                {
+                    nearbyButtons.Add("button_" + (activeIndex + 1));//Sağdaki komşu
+                }
+            }
+
+            int biggest = -1;
+            Button biggestButton = null;
+            foreach (String btn in nearbyButtons)
+	        {
+                int buttonVal = Convert.ToInt32(pnlButtons.Controls[btn].Text);
+                if (biggest == -1 || buttonVal > biggest)
+                {
+                    biggest = buttonVal;
+                    biggestButton = (Button) pnlButtons.Controls[btn];
+                }
+	        }
+
+            if (biggestButton == null)
+            {
+                tmr.Stop();
+            }
+            else
+            {
+                biggestButton.BackColor = Color.Red;
+                lastActiveButton = biggestButton;
+            }
+        }
+    }
+}
